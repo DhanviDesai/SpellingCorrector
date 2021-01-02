ca01a11e6d0173a88b41096f54170462642c2c79
DhanviDesai <dhanvidesai99@gmail.com>
2021-01-02 19:14:45 +0530

    Folder restructure

--- spelling_suggestor.py
+++ spelling_suggestor.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 #Calculate the edit distance to convert str1 to str2
 def editDistDP(str1, str2, m, n):
@@ -73,7 +74,7 @@ answer_list = []
 real_words_dict = {}
 
 #Read the dictionary and save the words
-with open("json_files\\dictionary-json.json","r") as openfile:
+with open(os.path.join(os.getcwd,"json_files","dictionary-json.json"),"r") as openfile:
     real_words_dict = json.load(openfile)
 
 #Create the list of words with atleast one bigram
